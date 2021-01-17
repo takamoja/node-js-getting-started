7479a36f31f9ac4c42869840c6d076f51223d020
Takayuki Mory <mojage_and_flower@me.com>
2021-01-17 23:52:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express')
 const basicAuth = require('basic-auth-connect')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
 const USERNAME = 'user'
 const PASSWORD = 'luser1'
 
-express()
-  .use(basicAuth(USERNAME, PASSWORD))
+app.use(basicAuth(USERNAME, PASSWORD))
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
