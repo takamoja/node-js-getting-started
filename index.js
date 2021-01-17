8dfa209e951adf3ff41b8a2ea6b5f1fb647e56ed
Takayuki Mory <mojage_and_flower@me.com>
2021-01-17 23:38:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const basicAuth = require('basic-auth-connect')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+const USERNAME = 'user'
+const PASSWORD = 'luser1'
+
+express()
+  .use(basicAuth(USERNAME, PASSWORD))
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
